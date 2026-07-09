6015c638b14888a1fc29ffe0d6a4142bcc7b60f4
Harrison Cohen <hardoc@unc.edu>
2026-07-09 15:03:10 -0500

    Add: permutation-string - submission-7

---
+++ Data Structures & Algorithms/permutation-string/submission-7.py
@@ -0,0 +1,11 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        l,r = 0, len(s1)
+
+        while r < len(s2)+1:
+            if sorted(s1) == sorted(s2[l:r]):
+                return True
+            r+=1
+            l+=1
+        return False
+        
