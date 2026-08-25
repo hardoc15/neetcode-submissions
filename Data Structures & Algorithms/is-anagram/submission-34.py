668ac35346c0fccd7358390fc6f050b954d7a1d9
Harrison Cohen <hardoc@unc.edu>
2026-08-25 00:11:07 -0400

    Add: is-anagram - submission-34

---
+++ Data Structures & Algorithms/is-anagram/submission-34.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        sdict = {}
+
+        tdict = {}
+
+        for i in s:
+            sdict[i] = 1 + sdict.get(i,0)
+
+        for i in t:
+            tdict[i] = 1 + tdict.get(i,0)
+        
+        return sdict == tdict
